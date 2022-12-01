ae0f6f8e2f67b669ea140dcebbdce97d924506ce
Brian Auron <brian.auron@oracle.com>
2022-11-30 21:32:03 -0800

    Day 01 complete

--- day01/src/part1/main.rs
+++ day01/src/part1/main.rs
@@ -2,4 +2,18 @@ fn main() {
     let opts = adventils::OptionalArgumentsAndSuchLike::args();
     let contents = adventils::get_input(opts.input_file.clone());
     // implement solutions in here somewhere
+    let mut vec = Vec::new();
+    let mut sum: i32 = 0;
+    for line in contents.lines() {
+        if line.is_empty() {
+            vec.push(sum);
+            sum = 0;
+            continue;
+        }
+        let line_num = line.parse::<i32>().unwrap();
+        sum += line_num;
+    }
+    vec.push(sum); // Push the last sum
+    let max_val = vec.iter().max();
+    println!("Max value: {}", max_val.unwrap().to_string());
 }
--- day01/src/part2/main.rs
+++ day01/src/part2/main.rs
@@ -2,4 +2,19 @@ fn main() {
     let opts = adventils::OptionalArgumentsAndSuchLike::args();
     let contents = adventils::get_input(opts.input_file.clone());
     // implement solutions in here somewhere
+    let mut vec = Vec::new();
+    let mut sum: i32 = 0;
+    for line in contents.lines() {
+        if line.is_empty() {
+            vec.push(sum);
+            sum = 0;
+            continue;
+        }
+        let line_num = line.parse::<i32>().unwrap();
+        sum += line_num;
+    }
+    vec.push(sum); // Push the last sum
+    vec.sort();
+    let top_three: i32 = vec.iter().rev().take(3).sum();
+    println!("Top three snack holders' calories summed together: {}", top_three.to_string());
 }
