30f46a715035a7a911ace92a7a57a7502b52e194
Brian Auron <brian.auron@oracle.com>
2022-11-30 20:54:19 -0800

    Refactoring slightly to allow for multiple parts with different inputs/logic.

---
+++ day01/src/part2/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let opts = adventils::OptionalArgumentsAndSuchLike::args();
+    let contents = adventils::get_input(opts.input_file.clone());
+    // implement solutions in here somewhere
+}
---
+++ daynaught/src/part1/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let opts = adventils::OptionalArgumentsAndSuchLike::args();
+    println!("Used adventils::Options::args to get program options and arguments...");
+    println!("Verbosity: {}", opts.verbose);
+    println!("Input file: {}", opts.input_file);
+    let contents = adventils::get_input(opts.input_file.clone());
+    println!("Contents of file {}:\n{}", opts.input_file, contents);
+    println!("Contents of file {} as lines:", opts.input_file);
+    for line in contents.lines() {
+        println!("\t{line}");
+    }
+    println!("Contents of file {} as lines, tokenized on ' ':", opts.input_file);
+    for line in contents.lines() {
+        let tokens: Vec<&str> = line.split(' ').collect();
+        println!("\ttokens:");
+        for token in tokens {
+            println!("\t\t{token}");
+        }
+    }
+}
